4c649daa5100482614075ccdbabad3c0a3d9af5e
Viviana Teng <viviiteng.-@hotmail.com>
2023-10-05 19:34:05 -0300

    cambios random

--- JAPFLIX/js/index.js
+++ JAPFLIX/js/index.js
@@ -8,25 +8,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const btn = document.getElementById("btnBuscar");
             const input = document.getElementById("inputBuscar");
+            
+            
             btn.addEventListener("click", (e) => {
                 e.preventDefault();
                 if (input.value) {
                     // campo buscador
                     const textoBusqueda = input.value.toLowerCase();
-
+                    const container=document.getElementById("lista");
                     // Filtrar los productos según el texto de búsqueda
                     const peliculasFiltradas = arrayPeliculas.filter((pelicula) => {
                         const titulo = pelicula.title.toLowerCase();
                         const descripcion = pelicula.overview.toLowerCase();
-                        const genero=pelicula.genres
-                        genero.forEach(element => {
-                            element
-                        });
-                        return titulo.includes(textoBusqueda) || descripcion.includes(textoBusqueda) || genero.includes(textoBusqueda);
+                        // const genero=pelicula.genres
+                        // genero.forEach(element => {
+                        //     element
+                        // });
+                        return titulo.includes(textoBusqueda) || descripcion.includes(textoBusqueda);
                     });
-                    console.log(peliculasFiltradas);
+                    peliculasFiltradas.forEach(element => {
+                        const item = document.createElement("div");
+                        item.classList.add("pelicula");
+                        const titulo = document.createElement("h5");
+                        titulo.textContent = element.title;
+                        item.appendChild(titulo);
+                        const tagline = document.createElement("p");
+                        tagline.textContent = element.tagline;
+                        item.appendChild(tagline);
+                        container.appendChild(item);
+                        // const stars = ScoreToEstrellas(comment.score);
+               
                     // Limpiar la lista y mostrar los productos filtrados
-
+                    });
                 } else {
                     alert("Campo de búsqueda vacío")
                 }
